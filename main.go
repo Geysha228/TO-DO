4ab226182f4770b03482ba954a04701033ac2468
Geysha <golovlev-2005@mail.ru>
2025-04-10 10:44:04 +0300

    Create type Task

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "github.com/labstack/echo/v4"
+
+type task struct{
+	id int32
+	text string
+}
+
+
 func main() {
+	//New instance of Echo
+	e := echo.New()
+
+
+}
+
+func getAllTasks() []task{
 
 }
