5ddd77ef23042a45c4de23bf6687212995b5faf1
Geysha <golovlev-2005@mail.ru>
2025-04-11 11:39:12 +0300

    Add read file by lines

--- logicForTxt/readFile.go
+++ logicForTxt/readFile.go
@@ -2,7 +2,7 @@ package logicfortxt
 
 import (
 	models "TO-DO/Models"
-	"io"
+	"bufio"
 	"log"
 	"os"
 	"strconv"
@@ -11,38 +11,61 @@ import (
 
 
 func ReadFileF(file os.File) []models.Task{
-	allText, err := io.ReadAll(&file)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	var bufForIndex strings.Builder
-	var bufForText strings.Builder
-	indexPoint := 0
+	countLines := CountOfLinesInFile(&file)
 
-	for i, val := range allText {
+	tasks := make([]models.Task, 0, countLines)
+
+	scanner := bufio.NewScanner(&file)
+
+	for scanner.Scan(){
+		line := scanner.Text()
+
+		var bufForIndex strings.Builder
+		var bufForText strings.Builder
+		indexPoint := 0
+
+
+		//Read Index
+		for i, val := range line {
 		if val == '.' {
 			indexPoint = i + 1
 			break
-		}
+			}
 		bufForIndex.WriteString(string(val))
-	}
-
-	for i := indexPoint; i < len(allText); i++ {
-		if allText[i] == '\n' {
-			break
 		}
-		bufForText.WriteString(string(allText[i]))
+
+		//Read Text
+		for i := indexPoint; i < len(line); i++ {
+		if line[i] == '\n' {
+			break
+			}
+		bufForText.WriteString(string(line[i]))
+		}
+
+		var task models.Task
+		var err error
+
+		task.ID, err = strconv.ParseInt(bufForIndex.String(), 10, 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+		task.Text = bufForText.String()
+
+
+		tasks = append(tasks, task)
 	}
 
-	var task models.Task
-	task.ID, err = strconv.ParseInt(bufForIndex.String(), 10, 32)
-	if err != nil {
-		log.Fatal(err)
-	}
-	task.Text = bufForText.String()
-
-	var tasks []models.Task
-	tasks = append(tasks, task)
 	return tasks
+}
+
+
+func CountOfLinesInFile(file *os.File) int32 {
+	scanner := bufio.NewScanner(file)
+	res := 0
+
+	for scanner.Scan(){
+		res++
+	}
+	return int32(res)
 }
